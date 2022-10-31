0bf48fb85a3ec899f51cabbb6db654938ad1ba87
Tanathip64070045 <64070045@it.kmitl.ac.th>
2022-10-31 20:28:04 +0700

    non

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) { 
  MainWindow p1 = new MainWindow();
  MainWindow p2 = new MainWindow();
- 
+ MainWindow p3 = new MainWindow();
  }
 }
