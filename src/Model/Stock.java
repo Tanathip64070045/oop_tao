acbe982512a823b02b21c6243d6153f4b30519df
Tanathip64070045 <64070045@it.kmitl.ac.th>
2022-12-13 16:01:32 +0700

    แก้ error

---
+++ src/Model/Stock.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Stock {
+    private int items;
+
+    public Stock(int items) {
+        this.items = items;
+    }
+    public int getItems() {
+        return items;
+    }
+    public void setItems(int items) {
+        this.items = items;
+    }
+}
